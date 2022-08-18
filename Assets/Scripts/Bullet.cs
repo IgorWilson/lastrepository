f038c6c1ef57e3b2b54b3eaac2ec3d043629406e
IgorWilson <51823250+IgorWilson@users.noreply.github.com>
2022-08-18 20:34:14 +0300

    попытка сделать стрельбу + поворот перса

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
-    // [SerializeField] private float speed = 1;
-    // private Vector2 movement = {1, 1};
-    void Start()
+    private Vector2 _direction = Vector2.zero;
+    private float _speed;
+    private bool _isCreated = false;
+    private Rigidbody2D _rigidbody;
+
+    public void CreateBullet(Vector2 direction, float speed)
     {
-        
+        _direction = direction;
+        _speed = speed;
+        _isCreated = true;
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.velocity = direction.normalized * speed;
+        Destroy(gameObject, 10f);
     }
 
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float _movementSpeed;
     private Rigidbody2D _rigidbody;
+    [SerializeField] private Bullet _bulletPrefab;
+    [SerializeField] private Transform _placeForShooting;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 dir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - _placeForShooting.position).normalized;
+            Bullet bullet = Instantiate(_bulletPrefab, _placeForShooting.position, Quaternion.identity);
+            bullet.CreateBullet(dir, 25);
+        }
+    }
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = Vector3.zero;
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (movement != Vector2.zero)
+        {
+            MoveCharacter(movement);
+        }
+    }
+
+
+    private void MoveCharacter(Vector2 movement)
+    {
+        _rigidbody.MovePosition(_rigidbody.position + movement.normalized * _movementSpeed * Time.fixedDeltaTime);
+    }
 
 }
